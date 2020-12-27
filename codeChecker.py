8d916476c20de5e4d2595b6bee661fcf2f770664
Tom-stack3 <76645845+Tom-stack3@users.noreply.github.com>
2020-12-27 12:53:45 +0200

    Update codeChecker.py

--- codeChecker.py
+++ codeChecker.py
@@ -34,21 +34,22 @@ def readAndInitPaths(path, lines):
 
 
 
-def printData(num_comments,num_blank_lines, num_space_lines):
+def printData(num_comments,num_blank_lines):
     num_lines = len(lines)
-    num_of_code_lines = num_lines - num_comments - num_blank_lines - num_space_lines
+    num_of_code_lines = num_lines - num_comments - num_blank_lines
     print(bcolors.BOLD + "total number of lines:", num_lines)
     print("number of comment lines:", num_comments)
     print("number of code lines:", num_of_code_lines)
     print("number of blank lines:", num_blank_lines)
 
     if num_comments != 0:
-        print("ratio of code/comments = ", num_of_code_lines / num_comments)
+        ratio = str(round(num_of_code_lines / num_comments, 3))
+        print("ratio of code/comments = ", ratio)
     if num_lines != 0:
         print("percentage of each from the total lines:")
-        print("comments takes {}%".format(round((num_comments / num_lines) * 100)))
-        print("code takes {}%".format(round((num_of_code_lines / num_lines) * 100)))
-        print("blank lines takes {}%".format(round((num_blank_lines / num_lines) * 100)))
+        print(" - comments takes {}%".format(round((num_comments / num_lines) * 100)))
+        print(" - code takes {}%".format(round((num_of_code_lines / num_lines) * 100)))
+        print(" - blank lines takes {}%".format(round((num_blank_lines / num_lines) * 100)))
     print(bcolors.ENDC)
     print(SEPERATOR)
 
@@ -95,6 +96,14 @@ def printEmptySpaceLine(firstWarning, line_num):
         printWarningHeader()
     printWarning(warning)
 
+def printTooManyBlankLines(firstWarning, line_num):
+    warning = 'found too many blank lines consecutively in line:'
+    warning += str(line_num)
+    if (firstWarning):
+        printWarningHeader()
+    printWarning(warning)
+
+
 def printTwoOneLineComments(firstWarning, line_num):
     warning = 'found two one line comments ( //bla bla ) one after another in line:'
     warning += str(line_num)
@@ -103,8 +112,8 @@ def printTwoOneLineComments(firstWarning, line_num):
     printWarning(warning)
 
 def work(lines):
+    num_blank_lines_in_row = 0
     num_comments = 0
-    num_space_lines = 0
     num_blank_lines = 0
     line_num = 1
     firstWarning = True
@@ -132,29 +141,35 @@ def work(lines):
             if len(line) < 4:
                 #check if empty space short line
                 if not re.search('[a-zA-Z0-9]', line):
+                    num_blank_lines_in_row += 1
                     printEmptySpaceLine(firstWarning,line_num)
                     firstWarning = False
-                    num_space_lines += 1
+                    num_blank_lines += 1
                 else:
+                    num_blank_lines_in_row = 0
                     printSpaceWrongUsage(firstWarning, line_num)
                     firstWarning = False
 
             # check is just an empty space
             elif not re.search('[a-zA-Z0-9]', line):
+                num_blank_lines_in_row += 1
                 printEmptySpaceLine(firstWarning, line_num)
                 firstWarning = False
-                num_space_lines += 1
+                num_blank_lines += 1
 
             #check is using the proper 4 spaces minimum
             elif line[0:4] != "    ":
+                num_blank_lines_in_row = 0
                 printSpaceWrongUsage(firstWarning, line_num)
                 firstWarning = False
 
         #we are in a comment block and the block hasn't ended
         elif isInCommentBlock and "*/" not in line:
+            num_blank_lines_in_row = 0
             num_comments += 1
         # we are in a comment block and the block ended
         elif isInCommentBlock and "*/" in line:
+            num_blank_lines_in_row = 0
             # check if there is text in line with the end of the comment block:
             # bla bla */ - not valid
             if re.search('[a-zA-Z0-9]', line):
@@ -166,13 +181,16 @@ def work(lines):
 
         #regular one line comment
         elif not isInCommentBlock and "//" in line:
+            num_blank_lines_in_row = 0
             num_comments += 1
             #if the previous one was also a one line comment (//bla bla)
             if line_num > 1 and "//" in lines[line_num-2]:
                 printTwoOneLineComments(firstWarning, line_num)
+                firstWarning = False
 
         #the block is actually one line
         elif "/*" in line and "*/" in line:
+            num_blank_lines_in_row = 0
             #not according to the coding style
             printCommentCodingStyle(firstWarning,line_num)
             firstWarning = False
@@ -180,6 +198,7 @@ def work(lines):
             isInCommentBlock = True
         #we start a comment block
         elif "/*" in line:
+            num_blank_lines_in_row = 0
             #check if there is text in line with the start of the comment block:
             # /*textHere - not valid
             if re.search('[a-zA-Z0-9]', line):
@@ -190,11 +209,19 @@ def work(lines):
             isInCommentBlock = True
         # line is empty from chars
         elif not re.search('[a-zA-Z0-9]', line):
+            num_blank_lines_in_row += 1
             #just a blank line
             if " " not in line:
                 num_blank_lines += 1
 
-        line_num+=1
+        #if there are more than 2 blank lines in row
+        if num_blank_lines_in_row > 2:
+            num_blank_lines_in_row = 0
+            printTooManyBlankLines(firstWarning,line_num)
+            firstWarning = False
+
+        line_num += 1
+
 
     #forbidden to use both tabs and spaces as indention
     if usingTab and usingSpaces:
@@ -208,7 +235,7 @@ def work(lines):
         print(SEPERATOR)
 
     print()
-    printData(num_comments,num_blank_lines,num_space_lines)
+    printData(num_comments,num_blank_lines)
 
 if __name__ == '__main__':
     finished = False
